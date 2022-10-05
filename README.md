3cc6c9b80ed1b68fd43c24872392b351fd4d95a3
Venustiano <v.soancatl.aguilar@rug.nl>
2022-10-05 21:15:50 +0200

    improved violin

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# testpackage
-Testing an R package
+
+# pcaprojection
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+This package includes a function to create a projection scatter plot using the first two principal components as a result fo applying Principal component analysis.
+
+## Installation
+
+You can install the development version of pcaprojection from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("Venustiano/testpackage")
+```
+
+## Example
+
+This is a basic example which shows you how to create projection:
+
+``` r
+library(pcaprojection)
+## basic example code
+```
+
---
+++ exec/cm_plots.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+library(pcaprojection)
+
+# test if there is at least two arguments: if not, return an error
+if (length(args)==0) {
+  stop("Both 'plot type' and a 'json file' arguments are needed", call.=FALSE)
+} else if (length(args)>=2) {
+  plottype <- args[1]
+  parameters <- args[2]
+  switch( plottype,
+          "histogram" = if (args[2] == "help")
+                          ?histogram
+                        else
+                          histogram(parameters),
+          "violin" = if (args[2] == "help")
+                      ?cm_ggviolin
+                     else
+                        cm_ggviolin(parameters),
+          "pca" = if (args[2] == "help")
+                    ?pcaproj
+                  else
+                    pcaproj(parameters),
+          print(paste0("'",plottype,"' plot is not found in this package"))
+          )
+}
+
+# docker container run --rm  -v "$PWD":/app/data pcap parameters.json
+# Rscript cm_plots.R histogram ../data/hist_parameters.json
+# Rscript cm_plots.R histogram help
